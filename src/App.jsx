c61ae06a2d5c5f60307c30f88185d9560e99f8ae
Rohit Kumar <rohit.r.kumar@newgensoft.com>
2024-03-21 12:21:51 +0530

    merging with productList

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// import './App.css'
-
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import {CssBaseline} from "@mui/material";
-import {BrowserRouter as Router} from "react-router-dom";
-import Header from './components/Header';
-import {Provider} from "react-redux";
-import { store } from './redux/store';
-import Productdetails from './pages/Productdetails';
-=======
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -23,18 +9,10 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Homepage from "./pages/Homepage";
 import { Toaster } from "react-hot-toast";
->>>>>>> 06bc68b523c5525c408cc849644ed5e8cd2d24d3
-
-function App() {
+ 
+export default function App() {
   return (
     <Provider store={store}>
-<<<<<<< HEAD
-          <CssBaseline />
-          <Router>
-            <Header/>
-            <Productdetails/>
-          </Router>
-=======
       <CssBaseline />
       <Router>
         <Header />
@@ -44,9 +22,6 @@ function App() {
         </Routes>
       </Router>
       <Toaster />
->>>>>>> 06bc68b523c5525c408cc849644ed5e8cd2d24d3
     </Provider>
   );
 }
-
-export default App;
